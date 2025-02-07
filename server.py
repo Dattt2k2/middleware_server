2868c7e4cde5a07178f0d807acc56d146a66f853
Nguyen Huu Dat <datttne484@gmail.com>
2025-02-07 15:23:08 +0700

    test

--- server.py
+++ server.py
@@ -169,4 +169,5 @@ def tao_don_hang():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))  # Dùng PORT từ Render
+    app.run(host='0.0.0.0', port=port)
